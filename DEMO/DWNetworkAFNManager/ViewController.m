bb7158036c0326492acb3bf3a15e7bfce37b7bd5
Wicky <codewicky@163.com>
2018-03-26 19:10:34 +0800

    Pod

---
+++ DEMO/DWNetworkAFNManager/ViewController.m
@@ -0,0 +1,34 @@
+//
+//  ViewController.m
+//  DWNetworkAFNManager
+//
+//  Created by Wicky on 2018/3/26.
+//  Copyright © 2018年 Wicky. All rights reserved.
+//
+
+#import "ViewController.h"
+#import "DWNetworkAFNManager.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    [DWNetworkAFNManager GET:@"https://www.easy-mock.com/mock/5ab8d2273838ca14983dc100/zdwApi/test" parameter:nil success:^(NSURLSessionDataTask * _Nonnull task, id  _Nullable responseObject) {
+        NSLog(@"success - %@",responseObject);
+    } failure:^(NSURLSessionDataTask * _Nullable task, NSError * _Nonnull error) {
+        NSLog(@"fail - %@",error);
+    }];
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
---
+++ DWNetworkAFNManager.podspec
@@ -0,0 +1,16 @@
+Pod::Spec.new do |s|
+s.name = 'DWNetworkAFNManager'
+s.version = '1.0.0'
+s.license = { :type => 'MIT', :file => 'LICENSE' }
+s.summary = '这是一个AFN3.0的轻量封装，可实现基本数据请求且不会造成内存泄漏。This is an AFN3.0 lightweight package that implements basic data requests without causing memory leaks.'
+s.homepage = 'https://github.com/CodeWicky/DWNetworkAFNManager'
+s.authors = { 'codeWicky' => 'codewicky@163.com' }
+s.source = { :git => 'https://github.com/CodeWicky/DWNetworkAFNManager.git', :tag => s.version.to_s }
+s.requires_arc = true
+s.ios.deployment_target = '7.0'
+s.source_files = 'DWNetworkAFNManager/**/{DWNetworkAFNManager}.{h,m}'
+s.frameworks = 'UIKit'
+
+s.dependency 'AFNetworking', '~> 3.0.0'
+
+end
